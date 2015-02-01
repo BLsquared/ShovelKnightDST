71a078703cc698369e1d43546ff8296c619c6dab
Brett Lane <commodore@brettlane.com>
2015-02-01 11:14:22 -0700

    Testing the Merging of Branches

--- modinfo.lua
+++ modinfo.lua
@@ -1,8 +1,8 @@
 -- This information tells other players more about the mod
-name = "Shovel Knight : Shovelblade_Update"
+name = "Shovel Knight : Shovelblade_Update2"
 description = "A character for Don't Starve Together, who wields a Shovelblade!"
 author = "BLsquared & Jade_KnightBlazer"
-version = "0.15.0"
+version = "1.0.0"
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
 forumthread = "/files/file/1035-character-shovel-knight-the-blue-burrower/"
