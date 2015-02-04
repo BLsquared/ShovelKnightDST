04105e36639fa72809dddd158388706f1dcc6afa
Brett Lane <commodore@brettlane.com>
2015-02-04 16:23:16 -0700

    Preparing to merge to Master

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 -- This information tells other players more about the mod
-name = "Shovel Knight : Shovelblade_Update"
+name = "Shovel Knight 0.16"
 description = "A character for Don't Starve Together, who wields a Shovelblade!"
 author = "BLsquared & Jade_KnightBlazer"
 version = "0.16.0"
