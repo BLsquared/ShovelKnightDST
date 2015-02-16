2582ffc791002d27c7ae6de5c4c0030c96cd84fa
Brett Lane <commodore@brettlane.com>
2015-02-16 16:28:16 -0700

    Remove Merging Conflicts

--- modinfo.lua
+++ modinfo.lua
@@ -2,11 +2,7 @@
 name = "Shovel Knight 0.17.1"
 description = "A character for Don't Starve Together, who wields a Shovelblade!"
 author = "BLsquared & Jade_KnightBlazer"
-<<<<<<< HEAD
-version = "0.16.1"
-=======
 version = "0.17.1"
->>>>>>> origin/Armor_UpdateRelease
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
 forumthread = "/files/file/1035-character-shovel-knight-the-blue-burrower/"
