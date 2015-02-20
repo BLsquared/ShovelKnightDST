c1120fb621dce8b7b6cded917441d058febe28ed
Brett Lane <commodore@brettlane.com>
2015-02-20 12:47:16 -0700

    New Version #
    Ready to Upload!

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@
 name = "Shovel Knight: Relic Update [Chalice]"
 description = "A character for Don't Starve Together, who wields a Shovelblade!"
 author = "BLsquared & Jade_KnightBlazer"
-version = "0.17.1"
+version = "0.18.0"
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
 forumthread = "/files/file/1035-character-shovel-knight-the-blue-burrower/"
