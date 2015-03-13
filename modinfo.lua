3b7e8c2b5a2505b7f5a095e20f5e9117e8762409
Brett Lane <commodore@brettlane.com>
2015-03-12 19:05:57 -0600

    Version #

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 -- This information tells other players more about the mod
-name = "Shovel Knight [Troupple Pond]"
+name = "Shovel Knight 0.20.0"
 description = "A character for Don't Starve Together, who wields a Shovelblade!"
 author = "BLsquared & Jade_KnightBlazer"
 version = "0.20.0"
